03af0e399fd478dd6fe46863c4ca53fcbda9a8c4
MirceaDonciu <mircea.donciu@gmail.com>
2023-04-17 15:01:10 +0300

    Home Integrated

--- app/src/views/home.tsx
+++ app/src/views/home.tsx
@@ -10,15 +10,33 @@ export const Home = ({ isLoaded, setIsLoaded, userId, setUserId }: any) => {
   };
   return (
     <div>
-      <section id="login">
-      {!isLoaded ? (
-        <Login setIsLoaded={setIsLoaded} setUserId={setUserId} />
-      ) : (
-        <div style={{ overflowY: "auto" }}>
-          <div className="sample-container"></div>
-        </div>
-      )}
-      </section>
+      <main>
+        <section id="Despre">
+          <p>Bun venit la siteul nostru de filme. Acesta este un spatiu pentru recenzii si discutii pe baza de filme.</p>
+          <p>Folositi bara de mai sus pentru a naviga la meniul de filme, si meniul din stanga pentru a va loga</p>
+        </section>
+        {/* <section id="Filme">
+          <ul><h3>Link pentru reviewuri la filmele noastre preferate</h3>
+            <li>Amadeus</li>
+            <li>Cetateanul Kane</li>
+            <li>Vertigo</li>
+            <li>Schindler's List</li>
+            <li>2001 A Space Odyssey</li>
+          </ul>
+        </section> 
+        <section id="mesajeRecente">
+          Cele mai populare mesaje in ultimele 24h:
+        </section>*/}
+        <section id="login">
+          {!isLoaded ? (
+            <Login setIsLoaded={setIsLoaded} setUserId={setUserId} />
+          ) : (
+            <div style={{ overflowY: "auto" }}>
+              <div className="sample-container"></div>
+            </div>
+          )}
+        </section>
+      </main>
       <footer>
         <h3>Contact:</h3>
         <ul>
