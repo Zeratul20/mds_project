df97842c6c598cce4cc9cd350964b57f131c5ec5
Vlad Andries <vlad.andries@code11.com>
2023-04-17 12:58:08 +0300

    add footer to home page

--- app/src/views/home.tsx
+++ app/src/views/home.tsx
@@ -21,6 +21,35 @@ export const Home = ({ isLoaded, setIsLoaded, userId, setUserId }: any) => {
           <div className="sample-container"></div>
         </div>
       )}
+      <footer>
+        <h3>Contact:</h3>
+        <ul>
+          <li>
+            Telefon: <a href="tel:+40712345678">0712345678</a>
+          </li>
+          <li>
+            <a href="https://www.google.com/maps?cid=17177553220990845266">
+              Locatia noastra
+            </a>
+          </li>
+          <li>
+            Email: <a href="mailto:ionel@gmail.com">mailto:ionel@gmail.com</a>
+          </li>
+          <li>
+            <a href="skype:user123?chat">Skype chat</a>
+          </li>
+        </ul>
+        <br />
+        <br />
+        <div id="copyright">
+          <p>
+            <small>Copyright &copy;2023</small>
+          </p>
+          <p>
+            <small>Donciu Mircea & Vlad Andries</small>
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
